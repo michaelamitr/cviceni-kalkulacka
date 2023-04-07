97ef1089d6324981d35f92591e463ae03c7de332
Michaela Trčková <misa.trckova@volny.cz>
2023-04-07 21:56:34 +0200

    final

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('funguju');
+
+const handleDigitClick = (event) => {
+  const clicked = event.target.textContent;
+  if (document.querySelector('.display').textContent.length < 9) {
+    if (document.querySelector('.display').textContent === '0') {
+      document.querySelector('.display').textContent = clicked;
+    } else {
+      document.querySelector('.display').textContent += clicked;
+    }
+  }
+};
+
+document.querySelector('#btn-0').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-1').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-2').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-3').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-4').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-5').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-6').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-7').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-8').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-9').addEventListener('click', handleDigitClick);
